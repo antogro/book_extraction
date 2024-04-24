9149c0cc0863efb2b7c34ee9cd10795e99f9bca9
Antonin Grau <antonin.grau35@gmail.com>
2024-04-24 18:04:47 +0200

    création de tout les fichier csv par catégorie, et ajout des urls dans le dict_csv

--- book_scraper.py
+++ book_scraper.py
@@ -1,32 +1,32 @@
  #importé les package necessaire pour récuperer les infos des livres
 import requests
 from bs4 import BeautifulSoup
-
+import pep8
 
 links = []
 #lien de la page d'accueille à scrapper
-urls = 'https://books.toscrape.com/index.html'
+urls = 'https://books.toscrape.com/catalogue/category/books/travel_2/index.html'
 
 
 
 #récuperer les lien de la 1er page à scrapper 
-def get_Url():
+def get_Url(urls):
     reponses = requests.get(urls)
     if reponses.ok:
     #On cherche tout les liens contenus dans la balise h3
-        soup = BeautifulSoup(reponses.text, 'html.parser')
+        soup = BeautifulSoup(reponses.content, 'html.parser')
         tdl = soup.findAll('h3')
-
         #On recuperer les liens de la 1er page
         for td in tdl:
             a = td.find('a')
             link = a['href']
+            link = link[9:]
             #   .get('href')
-            links.append('https://books.toscrape.com/' + link)
-    print(links)
+            links.append('https://books.toscrape.com/catalogue/' + link)
+    return links
 
-get_Url()
 
+get_Url(urls)
 
 #lien de la page a scrapper
 url = 'https://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html'
@@ -38,10 +38,10 @@ book_1 = ['Title', 'UCP', 'Price Excl TAX','Price Includ TAX', 'Nombre available
 #récuperer le titre d'un livre 
 def get_title ():
     if reponses.ok:
-        soup = BeautifulSoup(reponses.text, 'html.parser')
+        soup = BeautifulSoup(reponses.content, 'html.parser')
         title = soup.find('h1')
         title_book.append(title.get_text())
-        print(title_book)
+        return title_book
 
         
 #récuperer les informations de chaque livre
@@ -49,67 +49,88 @@ def get_title ():
 # number available,  
 def get_info_book():
     if reponses.ok:
-        soup = BeautifulSoup(reponses.text, 'html.parser')
+        soup = BeautifulSoup(reponses.content, 'html.parser')
 
         for i in soup.findAll('tr'):
             #recuperer le code UPC d'une page
             book = i.find_all('td')
 
             for td in book:
-                if td.get_text() in ['Books', '0', 'Â£0.00']:
+                if td.get_text() in ['Books', '0', '£0.00']:
                     td.extract()
                 else: 
-                    print(td.get_text())
+                    return td.get_text()
 
 # récuperer les category de chaque livre            
 def get_category_book():
     if reponses.ok:
-        soup = BeautifulSoup(reponses.text, 'html.parser')
+        soup = BeautifulSoup(reponses.content, 'html.parser')
         category = soup.find('ul', 'breadcrumb')
+#récuperer la catégorie se trouvant à la 3éme place de la liste    
         li_list = category.find_all('li', limit = 4)
+        
         if len(li_list) >=3:
-            print(li_list[2].get_text())
+            return (li_list[2].get_text())
 
+
+
+####################test#######################
+#récuperer le review rating de chaque livre
+product_description = []
+star_rating = []
+def get_star_rating():
+    if reponses.ok:
+        soup = BeautifulSoup(reponses.content, 'html.parser')
+        star = soup.findAll('p', 'star-rating')
+        if star:
+            star_rating = star[0] 
+            return (star_rating.get('class')[-1])
+#récuperer la description de chaque livre
+    if reponses.ok:
+        soup = BeautifulSoup(reponses.content, 'html.parser')
+        paragraphe_description = soup.find(id = 'content_inner')
+        product_description = paragraphe_description.find_all('p')[3]
+        return (product_description.get_text())
+ 
+####################test#########################
 
 #récuperer le review rating de chaque livre
 star_rating = []
 def get_star_rating():
     if reponses.ok:
-        soup = BeautifulSoup(reponses.text, 'html.parser')
+        soup = BeautifulSoup(reponses.content, 'html.parser')
         star = soup.findAll('p', 'star-rating')
         if star:
             star_rating = star[0] 
-            print(star_rating.get('class')[-1])
- 
+            return (star_rating.get('class')[-1])
+
 
 #récuperer la description de chaque livre
 product_description = []
 def get_description():
     if reponses.ok:
-        soup = BeautifulSoup(reponses.text, 'html.parser')
+        soup = BeautifulSoup(reponses.content, 'html.parser')
         paragraphe_description = soup.find(id = 'content_inner')
         product_description = paragraphe_description.find_all('p')[3]
-        print(product_description.get_text())
+        return (product_description.get_text())
 
 
 #récuperer l'Url de l'image de chaque livre
 url_image = []
 def get_image_url():
     if reponses.ok:
-        soup = BeautifulSoup(reponses.text, 'html.parser')
+        soup = BeautifulSoup(reponses.content, 'html.parser')
         image_url = soup.find(class_= 'item active')
         
         image = image_url.find('img')
         link_image = str(image['src'])
         link = link_image[5:]
-        print('https://books.toscrape.com/' + link)
-
+        return ('https://books.toscrape.com/' + link)
 
 
 get_image_url()
 
-get_Url()
-
+get_description()
 
 get_info_book()
 
@@ -119,7 +140,8 @@ get_category_book()
 
 get_star_rating()
 
-get_description()
+
+
 
 
 
--- category_scrapper.py
+++ category_scrapper.py
@@ -2,8 +2,10 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
-
-
+import pandas as pd
+import csv
+import book_scraper as bscrap
+import pep8
 
 
 urls = 'https://books.toscrape.com/catalogue/category/books/mystery_3/page-2.html'
@@ -16,7 +18,7 @@ def get_url_category():
     urls = 'https://books.toscrape.com/index.html'
     reponses = requests.get(urls)
     if reponses.ok: 
-        soup = BeautifulSoup(reponses.text, 'html.parser')
+        soup = BeautifulSoup(reponses.content, 'html.parser')
         tul = soup.find('ul', 'nav nav-list')
         li_list = tul.find_all('li')
         for li in li_list:
@@ -24,34 +26,87 @@ def get_url_category():
             link = a['href']
             #   .get('href')
             category_link.append('https://books.toscrape.com/' + link)
-            return category_link
+        return(category_link)
 
-get_url_category()           
+get_url_category()       
+
+#Ecrire les urls dans un fichier csv
+with open('csv/urls_category.csv', 'w') as file:
+    for link in category_link:
+        file.write(link + '\n')
+
+
+
+links = []
+
+#ouverture du fichier csv pour scraper les urls des livres
+#dans les différentes catégories 
+#récuperer tout les liens de toute les pages des différentes catégories
+def get_all_url():
+    with open('csv/urls_category.csv', 'r') as file:
+        for row in file:
+            url = row.strip()               
+            links = bscrap.get_Url(url)
+        for category in list(links):
+            with open(f'{category}.csv', 'w') as file:
+                writer = csv.DictWriter('url', fieldnames= links)
+                writer.writeheader()
+                
+                
+get_all_url()
+
+
+
           
-#Enregistrer les urls des catégories dans un fichier csv
-
-for url in category_link:
-    parsed_url = urlparse(url)
-    category_name = parsed_url.path.split('/')[-2]
-print(category_name) 
-
-
-books = [
-    {'Title' : '', 'UCP':'', 'Price Excl TAX': '','Price Includ TAX':'', 'Nombre available':'', 
-          'Review Rating' :'', 'Category': '', 'Produc description': '', 'URL' :''}]
-
-
-
-
-
-
+category_name = set()
+#récuperer toute les différentes catégories venant du CSV Url 
+def get_all_category_name():
     
-"""
+    with open('csv/urls_category.csv', 'r') as file:
+        for row in file:
+            url = row.strip()
+            reponses = requests.get(url)
+            parsed_url = urlparse(reponses.url)
+            category = parsed_url.path.split('/')[-2]
+            if category.endswith(('0','1','2','3','4','5','6','7','8','9')):
+                category = category[:-2]
+            if category.endswith('_'):
+                category = category[:-1]
+            if category not in category_name:
+                category_name.add(category)
+    return category_name
+    
+get_all_category_name()
+
+
+labels = ['Title', 'UCP', 'Price Excl TAX','Price Includ TAX', 'Nombre available', 
+          'Review Rating', 'Category', 'Produc description', 'URL']   
+
+
+#créée un fichier csv par catégorie avec le titre pour chaque donnée à scrapper
+def write_category_csv(category_name):
+    for category in list(category_name):
+        with open(f'csv/{category}.csv', 'w') as file:
+            writer = csv.DictWriter(file, fieldnames=labels)
+            writer.writeheader()
+            
+
+
+category_name = get_all_category_name()
+write_category_csv(category_name)
+
+
+
+
+
+
+
 
 #récuperer les autres pages
+
 def get_next_page(reponses):
     if reponses.ok:
-        soup = BeautifulSoup(reponses.text, 'html.parser')
+        soup = BeautifulSoup(reponses.content, 'html.parser')
         next_page = soup.find('li', 'next')
         if next_page == None :
             return
@@ -59,27 +114,3 @@ def get_next_page(reponses):
             a = next_page.find('a')
             next_page_link = a['href']
             return next_page_link
-      
-
-
-
-
-#ouverture du fichier csv pour scraper les urls des livres
-#dans les différentes catégories 
-def get_all_page_category():
-    with open('urls_category.csv', 'r') as file:
-        for row in file:
-            url = row.strip()
-        reponses = requests.get(url)
-        if reponses.ok:
-            for link in get_next_page(reponses):
-                for each with open('urls_category_books.csv', 'w') as file:
-get_all_page_category()
-
-
-with open('urls_category.csv', 'w') as file:
-    for link in category_link:
-        file.write(link + '\n')
-"""
-
-
